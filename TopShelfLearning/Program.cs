9a3aa5167d6090ec44cf002e095215a91fa14688
Alberto Monteiro <alberto.monteiro@live.com>
2015-10-03 11:26:27 -0300

    Created simple service!! TopShelf FTW

--- TopShelfLearning/Program.cs
+++ TopShelfLearning/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Topshelf;
 
 namespace TopShelfLearning
 {
@@ -10,6 +8,65 @@ namespace TopShelfLearning
     {
         static void Main(string[] args)
         {
+            var host = HostFactory.New(x =>
+            {
+                x.SetServiceName("SampleServiceWithTopShelf");
+                x.Service<UhhMyService>();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Sample service with TopShelf");
+                x.SetDisplayName("Sample Service With TopShelf");
+                x.SetServiceName("SampleServiceWithTopShelf");
+                x.EnablePauseAndContinue();
+                x.EnableShutdown();
+            });
+            host.Run();
         }
+    }
+
+    public class UhhMyService : ServiceControl, IWindowsService
+    {
+        private readonly Timer _timer;
+
+        public UhhMyService()
+        {
+            _timer = new Timer(state =>
+            {
+                Console.WriteLine($"{DateTime.Now:G}");
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+        }
+
+        public void Start()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Start");
+            Console.ResetColor();
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Stop");
+            Console.ResetColor();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Start();
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Stop();
+            return true;
+        }
+    }
+
+    public interface IWindowsService
+    {
+        void Start();
+        void Stop();
     }
 }
